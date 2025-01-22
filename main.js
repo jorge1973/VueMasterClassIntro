04ae1c4a6c81459a8599f48b73f2cb0cc66bc330
jorge1973 <jediazf@protonmail.com>
2025-01-22 18:57:56 -0500

    list rendering

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             inventory: 100,
             onSale:true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {id:2234, color:'green', image: './assets/images/socks_green.jpg'},
+                {id:2235, color:'blue', image: './assets/images/socks_blue.jpg'}
+            ],
+            sizes: ['S', 'M', 'L', 'XL'],
         }
     }
 })

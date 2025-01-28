5050e5e851c852dac639f94ec4877fb98fb6a738
jorge1973 <jediazf@protonmail.com>
2025-01-27 19:51:05 -0500

    Eventhandling

--- main.js
+++ main.js
@@ -6,12 +6,24 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             inventory: 100,
             onSale:true,
+            cart:0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
                 {id:2234, color:'green', image: './assets/images/socks_green.jpg'},
                 {id:2235, color:'blue', image: './assets/images/socks_blue.jpg'}
             ],
             sizes: ['S', 'M', 'L', 'XL'],
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        removeFromCart(){
+            this.cart -= 1
         }
     }
 })

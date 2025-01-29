eed28542435aa9495873738b34c5f3a6f661dd0a
jorge1973 <jediazf@protonmail.com>
2025-01-28 19:59:51 -0500

    commitcontarea

--- main.js
+++ main.js
@@ -3,15 +3,16 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             description: 'A pair of warm, fuzzy ugly socks',
-            image: './assets/images/socks_green.jpg',
+            brand:'Vue Mastery',
+            selectedVariant:0,
             inventory: 100,
-            inStock:true,
+            // inStock:true,
             onSale:true,
             cart:0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                {id:2234, color:'green', image: './assets/images/socks_green.jpg'},
-                {id:2235, color:'blue', image: './assets/images/socks_blue.jpg'}
+                {id:2234, color:'green', image: './assets/images/socks_green.jpg', quantity:50},
+                {id:2235, color:'blue', image: './assets/images/socks_blue.jpg', quantity:0}
             ],
             sizes: ['S', 'M', 'L', 'XL'],
         }
@@ -23,8 +24,32 @@ const app = Vue.createApp({
         updateImage(variantImage){
             this.image = variantImage
         },
+        updateVariant(index){
+            this.selectedVariant = index
+        },
         removeFromCart(){
-            this.cart -= 1
+            if (this.cart > 0){
+                this.cart -= 1
+            }
+                
         }
-    }
+    },
+    computed:{
+        title(){
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        },
+        saleMessage(){
+            if (this.onSale){
+                return this.brand + ' ' + this.product + ' are on sale!'
+            }
+            return this.brand + ' ' + this.product + ' are not on sale'
+        },
+
+    },
 })

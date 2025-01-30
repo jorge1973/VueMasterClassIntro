fb288c3311d2b0fb7ef996903c6c9a681c8ade18
jorge1973 <jediazf@protonmail.com>
2025-01-30 12:09:33 -0500

    practica components y props

---
+++ components/ProductDisplay.js
@@ -0,0 +1,109 @@
+app.component('product-display', {
+    
+    props:{
+        premium:{
+            type: Boolean,
+            required: true
+        }
+    },
+    // The template property is used to define the HTML structure of the component.
+    template:
+
+    /*html*/
+    `
+    <div class="product-display">
+          <div class="product-container">
+            <div class="product-image">
+              <!-- Se puede usar v-bind o : para enlazar atributos -->
+              <!-- <img v-bind:src="image" alt="product" /> -->
+              <img :src="image" alt="product" />
+            </div>
+            <div class="product-info">
+              <h1>{{title}}</h1>
+              <p>{{description}}</p>
+              <!-- <p v-if="inventory>10">In Stock</p>
+              <p v-else-if="inventory>0 && inventory<=10">Almost sold out</p>
+              <p v-else>Out of Stock</p> -->
+              <p v-if="inStock">In Stock</p>
+              <p v-else>Out of Stock</p>
+              <p v-if="onSale">On Sale</p>
+              <!-- The v-for directive is a powerful tool in Vue.js for rendering lists of items. 
+               It helps in creating dynamic and efficient UIs by iterating over arrays or objects and rendering the corresponding HTML elements. 
+               The key attribute is crucial for maintaining performance and ensuring that the DOM updates efficiently. -->
+
+               <p>Shipping: {{shipping}}</p>
+
+              <ul>
+                <li v-for="detail in details" :key="detail">{{detail}}</li>
+              </ul> 
+              
+              <div v-for="(variant, index) in variants" :key="variant.id" @mouseover="updateVariant(index)" class="color-circle" :style="{backgroundColor: variant.color}">
+                
+              </div>
+              <div v-for="size in sizes" :key="size">{{size}}</div>
+              <button class="button" :class="{disabledButton:!inStock}" @click="addToCart" :disabled="!inStock">Add to Cart</button>
+              <button class="button" :disabled="!inStock" @click="removeFromCart" :class="{disabledButton:cart<=0}">Remove from Cart</button>
+            </div>
+          </div>
+        </div>
+        `
+    ,
+    data:function(){
+        return {
+            product: 'Socks',
+            description: 'A pair of warm, fuzzy ugly socks',
+            brand:'Vue Mastery',
+            selectedVariant:0,
+            inventory: 100,
+            // inStock:true,
+            onSale:true,
+            cart:0,
+            variants: [
+                {id:2234, color:'green', image: './assets/images/socks_green.jpg', quantity:50},
+                {id:2235, color:'blue', image: './assets/images/socks_blue.jpg', quantity:0}
+            ],
+            sizes: ['S', 'M', 'L', 'XL'],
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart += 1
+        },
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        updateVariant(index){
+            this.selectedVariant = index
+        },
+        removeFromCart(){
+            if (this.cart > 0){
+                this.cart -= 1
+            }
+                
+        }
+    },
+    computed:{
+        title(){
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        },
+        saleMessage(){
+            if (this.onSale){
+                return this.brand + ' ' + this.product + ' are on sale!'
+            }
+            return this.brand + ' ' + this.product + ' are not on sale'
+        },
+        shipping(){
+            if (this.premium){
+                return 'Free'
+            }
+            return 2.99
+        },
+
+    },
+})
